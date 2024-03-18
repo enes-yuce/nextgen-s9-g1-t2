1395533a490e1b81dab984f020fa635362f705e2
Gökhan <gn.ozdemir@gmail.com>
2024-03-18 19:01:50 +0300

    add toastify

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,42 @@ import TaskForm from './components/TaskHookForm';
 import TaskHookForm from './components/TaskHookForm';
 import PeopleForm from './components/PeopleForm';
 import { initialTasks, initialTeam } from './data';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [tasks, setTasks] = useState(initialTasks);
   const [team, setTeam] = useState(initialTeam);
 
   function handleTaskSubmit(yeniTask) {
+    toast("Yeni görev oluşturuldu.");
+
     setTasks([yeniTask, ...tasks]);
   }
 
   function handlePeopleSubmit(yeniKisi) {
+    toast("Yeni kişi oluşturuldu.");
     setTeam([...team, yeniKisi]);
   }
 
   function handleComplete(id) {
-    console.log('tamamlama fonksiyonunu buraya yazın');
+    const clonedTasks = [...tasks]
+
+    const updatedTasks = clonedTasks.map((t) => {
+      if (t.id === id) {
+        t.status = "yapıldı"
+      }
+      return t
+    })
+    setTasks(updatedTasks)
+    // toast(`${id} idli görev tamamlandı.`)
+    toast("2 idli görev tamamlandı.")
+    console.log('tamamlama fonksiyonunu buraya yazın', id);
   }
 
   return (
     <div className="app">
+      <ToastContainer />
       <div className="formColumn">
         <div className="form-container">
           <h2>Yeni Task</h2>
