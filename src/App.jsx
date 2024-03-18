0834d9d8a5b591a1d288aa3517c849700590243b
Gökhan <gn.ozdemir@gmail.com>
2024-03-18 18:21:47 +0300

    duplicate exisiting form to TaskHookForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './app.css';
 import Task from './components/Task';
+import TaskForm from './components/TaskHookForm';
 import TaskHookForm from './components/TaskHookForm';
 import PeopleForm from './components/PeopleForm';
 import { initialTasks, initialTeam } from './data';
@@ -26,7 +27,7 @@ function App() {
       <div className="formColumn">
         <div className="form-container">
           <h2>Yeni Task</h2>
-          {/* <TaskForm kisiler={team} submitFn={handleTaskSubmit} /> */}
+          <TaskForm kisiler={team} submitFn={handleTaskSubmit} />
           <TaskHookForm kisiler={team} submitFn={handleTaskSubmit} />
         </div>
 
